e8ae870875b85db76da64cef2b0cc80459670f96
Akanksh R V <119801233+akankshrv@users.noreply.github.com>
2024-12-29 22:08:20 -0800

    Update script.js

--- build_server/script.js
+++ build_server/script.js
@@ -5,7 +5,7 @@ const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 const mime = require("mime-types");
 
 const s3CLient = new S3Client({
-  region: "us-east-2",
+  region: "",
   credentials: {},
 });
 
